66f17e4e2dcbd1063dabc57182f8629706bb14d4
Steve <steve@codefactoring.com>
2016-03-13 20:51:51 +0100

    feat : Add Get User Icon Endpoint
    Added Get User Icon Endpoint call
    
    #Task 5 Implement Issue screen

--- backlog-api/src/main/java/com/codefactoring/android/backlogapi/operations/UserOperations.java
+++ backlog-api/src/main/java/com/codefactoring/android/backlogapi/operations/UserOperations.java
@@ -4,7 +4,9 @@ import com.codefactoring.android.backlogapi.models.User;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import rx.Observable;
 
 public interface UserOperations {
@@ -14,4 +16,7 @@ public interface UserOperations {
 
     @GET("users")
     Observable<List<User>> getUserList();
+
+    @GET("users/{userId}/icon")
+    Observable<ResponseBody> getUserIcon(@Path("userId") long id);
 }
