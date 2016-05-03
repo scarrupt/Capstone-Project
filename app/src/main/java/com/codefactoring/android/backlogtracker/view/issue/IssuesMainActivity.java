2ff01f81522f53ad01cac75f4d27837aca0723d5
Steve Carrupt <steve@codefactoring.com>
2016-05-03 10:54:00 +0900

    feat :  Add Issues Main fragment
    # Task 11: Add Tablet layouts

--- app/src/main/java/com/codefactoring/android/backlogtracker/view/issue/IssuesMainActivity.java
+++ app/src/main/java/com/codefactoring/android/backlogtracker/view/issue/IssuesMainActivity.java
@@ -3,11 +3,6 @@ package com.codefactoring.android.backlogtracker.view.issue;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -21,14 +16,6 @@ import com.google.android.gms.analytics.Tracker;
 
 import javax.inject.Inject;
 
-import butterknife.ButterKnife;
-
-import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_IN_PROGRESS;
-import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_OPEN;
-import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_RESOLVED;
-import static com.codefactoring.android.backlogtracker.provider.BacklogContract.IssuePreviewEntry;
-import static com.codefactoring.android.backlogtracker.provider.BacklogContract.IssueStatsEntry;
-
 public class IssuesMainActivity extends AppCompatActivity implements IssueListFragment.OnFragmentInteractionListener {
 
     @Inject
@@ -39,6 +26,7 @@ public class IssuesMainActivity extends AppCompatActivity implements IssueListFr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issues_main);
         setTitle(R.string.title_activity_issues_main);
+        initializeDependencyInjector();
 
         if (getIntent() != null && getIntent().hasExtra(Intent.EXTRA_TEXT)) {
             final String projectKey = getIntent().getStringExtra(Intent.EXTRA_TEXT);
@@ -48,13 +36,13 @@ public class IssuesMainActivity extends AppCompatActivity implements IssueListFr
             }
         }
 
-        initializeDependencyInjector();
-
-        final ViewPager viewPager = ButterKnife.findById(this, R.id.pager_issues);
-        final FragmentPagerAdapter pageAdapter = new IssuesMainPageAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(pageAdapter);
-        final TabLayout tabLayout = ButterKnife.findById(this, R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        if (savedInstanceState == null && getIntent() != null) {
+            final IssuesMainFragment issuesMainFragment = IssuesMainFragment.newInstance(getIntent().getData());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.issues_main_container, issuesMainFragment)
+                    .commit();
+        }
 
         mTracker.setScreenName(IssuesMainActivity.class.getSimpleName());
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
@@ -88,69 +76,5 @@ public class IssuesMainActivity extends AppCompatActivity implements IssueListFr
         final Intent intent = new Intent(this, IssueDetailActivity.class).setData(uri);
         intent.putExtra(Intent.EXTRA_TEXT, issueKey);
         startActivity(intent);
-    }
-
-    private class IssuesMainPageAdapter extends FragmentPagerAdapter {
-
-        private static final int TAB_COUNT = 4;
-
-        private static final int TAB_OPEN_ISSUES = 0;
-
-        private static final int TAB_IN_PROGRESS_ISSUES = 1;
-
-        private static final int TAB_RESOLVED_ISSUES = 2;
-
-        private static final int TAB_ISSUE_STATS = 3;
-
-        public IssuesMainPageAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case TAB_OPEN_ISSUES: {
-                    final Uri filteredUri = IssuePreviewEntry.addStatusQueryParameterToUri(getIntent().getData(),
-                            STATUS_ISSUE_OPEN);
-                    return IssueListFragment.newInstance(filteredUri);
-                }
-                case TAB_IN_PROGRESS_ISSUES: {
-                    final Uri filteredUri = IssuePreviewEntry.addStatusQueryParameterToUri(getIntent().getData(),
-                            STATUS_ISSUE_IN_PROGRESS);
-                    return IssueListFragment.newInstance(filteredUri);
-                }
-                case TAB_RESOLVED_ISSUES: {
-                    final Uri filteredUri = IssuePreviewEntry.addStatusQueryParameterToUri(getIntent().getData(),
-                            STATUS_ISSUE_RESOLVED);
-                    return IssueListFragment.newInstance(filteredUri);
-                }
-                case TAB_ISSUE_STATS:
-                    final String projectId = getIntent().getData().getQueryParameter(IssueStatsEntry.QUERY_PARAMETER_PROJECT_ID);
-                    return IssueStatsFragment.newInstance(IssueStatsEntry.buildIssueStatsUriWithProjectId(projectId));
-                default:
-                    throw new IllegalArgumentException();
-            }
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case TAB_OPEN_ISSUES:
-                    return IssuesMainActivity.this.getString(R.string.title_open_issues);
-                case TAB_IN_PROGRESS_ISSUES:
-                    return IssuesMainActivity.this.getString(R.string.title_in_progress_issues);
-                case TAB_RESOLVED_ISSUES:
-                    return IssuesMainActivity.this.getString(R.string.title_resolved_issues);
-                case TAB_ISSUE_STATS:
-                    return getString(R.string.title_issue_stats);
-                default:
-                    throw new IllegalArgumentException();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return TAB_COUNT;
-        }
     }
 }
---
+++ app/src/main/java/com/codefactoring/android/backlogtracker/view/issue/IssuesMainFragment.java
@@ -0,0 +1,126 @@
+package com.codefactoring.android.backlogtracker.view.issue;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codefactoring.android.backlogtracker.R;
+import com.codefactoring.android.backlogtracker.provider.BacklogContract;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_IN_PROGRESS;
+import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_OPEN;
+import static com.codefactoring.android.backlogapi.BacklogApiConstants.STATUS_ISSUE_RESOLVED;
+
+public class IssuesMainFragment extends Fragment {
+
+    private static final String ARG_URI = "issueListUri";
+
+    @Bind(R.id.pager_issues)
+    ViewPager viewPager;
+
+    @Bind(R.id.sliding_tabs)
+    TabLayout tabLayout;
+
+    public static IssuesMainFragment newInstance(Uri uri) {
+        final IssuesMainFragment fragment = new IssuesMainFragment();
+        final Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_issues_main, container, false);
+        ButterKnife.bind(this, rootView);
+
+        viewPager.setAdapter(new IssuesMainPageAdapter(getChildFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+
+    private class IssuesMainPageAdapter extends FragmentPagerAdapter {
+
+        private static final int TAB_COUNT = 4;
+
+        private static final int TAB_OPEN_ISSUES = 0;
+
+        private static final int TAB_IN_PROGRESS_ISSUES = 1;
+
+        private static final int TAB_RESOLVED_ISSUES = 2;
+
+        private static final int TAB_ISSUE_STATS = 3;
+
+        public IssuesMainPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            final Uri uri = getArguments().getParcelable(ARG_URI);
+
+            if (uri != null) {
+                switch (position) {
+                    case TAB_OPEN_ISSUES: {
+                        final Uri filteredUri = BacklogContract.IssuePreviewEntry.addStatusQueryParameterToUri(uri,
+                                STATUS_ISSUE_OPEN);
+                        return IssueListFragment.newInstance(filteredUri);
+                    }
+                    case TAB_IN_PROGRESS_ISSUES: {
+                        final Uri filteredUri = BacklogContract.IssuePreviewEntry.addStatusQueryParameterToUri(uri,
+                                STATUS_ISSUE_IN_PROGRESS);
+                        return IssueListFragment.newInstance(filteredUri);
+                    }
+                    case TAB_RESOLVED_ISSUES: {
+                        final Uri filteredUri = BacklogContract.IssuePreviewEntry.addStatusQueryParameterToUri(uri,
+                                STATUS_ISSUE_RESOLVED);
+                        return IssueListFragment.newInstance(filteredUri);
+                    }
+                    case TAB_ISSUE_STATS:
+                        final String projectId = uri.getQueryParameter(BacklogContract.IssueStatsEntry.QUERY_PARAMETER_PROJECT_ID);
+                        return IssueStatsFragment.newInstance(BacklogContract.IssueStatsEntry.buildIssueStatsUriWithProjectId(projectId));
+                    default:
+                        throw new IllegalArgumentException();
+                }
+            }
+
+            throw new IllegalArgumentException();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case TAB_OPEN_ISSUES:
+                    return IssuesMainFragment.this.getString(R.string.title_open_issues);
+                case TAB_IN_PROGRESS_ISSUES:
+                    return IssuesMainFragment.this.getString(R.string.title_in_progress_issues);
+                case TAB_RESOLVED_ISSUES:
+                    return IssuesMainFragment.this.getString(R.string.title_resolved_issues);
+                case TAB_ISSUE_STATS:
+                    return getString(R.string.title_issue_stats);
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+    }
+}
