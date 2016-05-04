90cfa440ae7d4ad20f06ef6fcda8cbc1eea2ffb7
Steve Carrupt <steve@codefactoring.com>
2016-05-04 23:49:50 +0900

    fix :  Add missing Settings menu

--- app/src/main/java/com/codefactoring/android/backlogtracker/view/issue/IssueDetailActivity.java
+++ app/src/main/java/com/codefactoring/android/backlogtracker/view/issue/IssueDetailActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ShareCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codefactoring.android.backlogtracker.BacklogTrackerApplication;
@@ -100,10 +101,16 @@ public class IssueDetailActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                supportFinishAfterTransition();
+                finish();
                 return true;
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
